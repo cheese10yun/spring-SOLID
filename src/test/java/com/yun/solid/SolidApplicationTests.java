963abe5617303723ace537ec9ab4241c02a01ca9
Yun <cheese10yun@gmail.com>
2018-07-23 03:52:21 +0900

    메인 클래스 테스트 변경

--- src/test/java/com/yun/solid/SolidApplicationTests.java
+++ src/test/java/com/yun/solid/SolidApplicationTests.java
@@ -2,11 +2,11 @@ package com.yun.solid;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SolidApplicationTests {
 
     @Test
